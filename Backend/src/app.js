3631fa38a0981fb7326c4e7de8ac55936dd2dd3d
Aman Kashyap <amankashyap0246@gmail.com>
2023-06-07 08:22:11 +0530

    link

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,10 @@
 const express  = require('express')
 const app = express()
 var cors = require('cors')
-app.use( res.header("Access-Control-Allow-Headers", "https://urlshortner-aman.onrender.com"))
+app.use( res.header("Access-Control-Allow-Headers", "https://url-shrinker-adnc.vercel.app"))
 app.use(cors({
     credentials:true,
-    origin:"http://localhost:3000"  
+    origin:"https://url-shrinker-adnc.vercel.app"  
 }))
 
 const routes = require('./routes/route.js')
